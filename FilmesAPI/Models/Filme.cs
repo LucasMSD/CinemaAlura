0bd641c1d432958ec12ef7acca4c26d0925587fe
Lucas Mariano dos Santos <lucasmsantos0153@gmail.com>
2022-05-03 16:49:03 -0300

    Implementando a funcionalidade de adicionar um novo filme. E configuração no lauchSettings para não abrir automaticamente o navegador ao executar a aplicação.

---
+++ FilmesAPI/Models/Filme.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models
+{
+    public class Filme
+    {
+        [Required(ErrorMessage = "O Título é um campo obrigatório.")]
+        public string Titulo { get; set; }
+        [Required(ErrorMessage = "O Título é um campo obrigatório.")]
+        public string Diretor { get; set; }
+        [Required(ErrorMessage = "O Título é um campo obrigatório.")]
+        public string Genero { get; set; }
+        [Range(1, 600, ErrorMessage = "A duração deve ter no mínimo 1 minuto e no máximo 600 minutos.")]
+        public int Duracao { get; set; }
+    }
+}
