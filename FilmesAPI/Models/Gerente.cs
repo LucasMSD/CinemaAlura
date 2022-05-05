7a431d2a6e5957e4ccbd67dd99c061aa0732beef
Lucas Mariano dos Santos <lucasmsantos0153@gmail.com>
2022-05-05 09:54:59 -0300

    Adicioando o relacionamento de 1:n de cinema e gerente.

--- FilmesAPI/Models/Gerente.cs
+++ FilmesAPI/Models/Gerente.cs
@@ -9,5 +9,6 @@ namespace FilmesAPI.Models
         public int Id { get; set; }
         [Required(ErrorMessage = "O campo de nome é obrigatório.")]
         public string Nome { get; set; }
+        public virtual List<Cinema> Cinemas { get; set; }
     }
 }
